d2d3ec5beb94b8a5cce0d670bc6252514a644b2c
Jaya <jayarke890@gmail.com>
2024-04-03 19:52:40 +0530

    first

---
+++ server/connect/getENV.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+// fetching all the Environment variables
+exports.db_url = 'mongodb://localhost:27017/scalar'
+exports.db_port = 5000
+exports.db_email = 'for.project.use12@gmail.com'
+exports.dp_email_pass = 'bkbk qjoj fyxb ieol';
